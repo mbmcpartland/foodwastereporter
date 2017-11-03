093ce56917b5c472787153630cfc76869207dbf6
Mohamed <maelafifi@dons.usfca.edu>
2017-11-02 17:06:19 -0700

    New Project

---
+++ client/src/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { BrowserRouter} from 'react-router-dom'
+//import ConnectedSearch from './components/Search'
+import { Route } from 'react-router'
+// import createHistory from 'history/createBrowserHistory'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import createSagaMiddleware from 'redux-saga'
+import mainReducer from './Reducers'
+//import watchFetchSearchData from './Sagas.js'
+
+
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import 'react-bootstrap-table/dist/react-bootstrap-table-all.min.css'
+
+
+//saga middleware
+const sagaMiddleware = createSagaMiddleware()
+
+//redux store with saga middleware
+const store = createStore(
+  mainReducer,
+  applyMiddleware(sagaMiddleware)
+)
+// activate the saga(s)
+// sagaMiddleware.run(watchFetchSearchData)
+
+// //fetch initial data
+// store.dispatch({type: 'FETCH_SEARCH_DATA', payload:{firstName: "*"}})
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <div>
+      <Route path='/' component={App}> </Route>
+      </div>
+    </BrowserRouter>
+    </Provider>,
+    document.getElementById('root')
+);
